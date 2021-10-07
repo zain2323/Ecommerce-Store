9f76ff33e6c13025cc62249918c13fc561839f3e
zain2323 <zainsiddiqui2323@gmail.com>
2021-10-07 14:01:26 +0500

    updated run.py file

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from EcommerceStore import create_app
 from flask import g
 from EcommerceStore.models import Category
 
-app = create_app()
+app = create_app(h)
 
 if __name__ == "__main__":
     app.jinja_env.globals['float'] = float
